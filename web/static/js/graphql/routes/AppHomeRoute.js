d80fbab01b717e0546ac74d565718f6cc7712c07
Yasuhiro Okada <yasuhiro.okada@aktsk.jp>
2015-11-17 18:10:41 +0900

    added arguments sample

--- web/static/js/graphql/routes/AppHomeRoute.js
+++ web/static/js/graphql/routes/AppHomeRoute.js
@@ -3,7 +3,7 @@ import Relay from 'react-relay';
 export default class extends Relay.Route {
   static path = '/';
   static queries = {
-    feeds: () => Relay.QL`query { feeds }`,
+    feeds: () => Relay.QL`query { feeds(count: 5) }`,
   };
   static routeName = 'AppHomeRoute';
 }
