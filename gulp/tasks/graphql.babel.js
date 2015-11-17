bc6c6501f9611a0a2f2b416e291f740dabdc2803
Yasuhiro Okada <yasuhiro.okada@aktsk.jp>
2015-11-17 18:09:48 +0900

    fixed

--- gulp/tasks/graphql.babel.js
+++ gulp/tasks/graphql.babel.js
@@ -25,7 +25,7 @@ let dirWebJs = dirWebStatic + '/js/graphql',
 
 let sourceFile = dirWebJs + '/app.js',
   destFolder = dirDestJs,
-  destJsRest = 'graphql.js';
+  destJsGraphql = 'graphql.js';
 
 let bundler = watchify(browserify({
   entries: [sourceFile],
@@ -44,7 +44,7 @@ bundler.on('log', $.util.log);
 function rebundle() {
   return bundler.bundle()
     .on('error', $.util.log.bind($.util, 'Browserify Error'))
-    .pipe(source(destJsRest))
+    .pipe(source(destJsGraphql))
     .pipe(buffer())
     .pipe(sourcemaps.init({loadMaps: true}))
     .pipe(sourcemaps.write('./'))
@@ -58,10 +58,10 @@ gulp.task('buildScriptsGraphQl', () => {
   return browserify({
     entries: sourceFile,
     paths: ['.'],
-    transform: [babelify]
+    transform: [babelify.configure( {plugins: [getbabelRelayPlugin(schema.data)]} )]
   })
     .bundle()
-    .pipe(source(destJsRest))
+    .pipe(source(destJsGraphql))
     .pipe(gulp.dest(dirDestJs));
 });
 
