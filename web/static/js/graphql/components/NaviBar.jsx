5e0d5984f3c147122273e92d56280b6da99d83eb
Yas Okada <yas.okadatech@gmail.com>
2015-12-01 07:03:18 +0900

    navi

---
+++ web/static/js/graphql/components/NaviBar.jsx
@@ -0,0 +1,44 @@
+import React, { Component, PropTypes } from 'react';
+import { Router, Route, Link } from 'react-router'
+
+export default class NavBar extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <nav className="navbar navbar-default">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse"
+                    data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+              <span className="sr-only">Toggle navigation</span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+            </button>
+            <a className="navbar-brand" href="#">Relay</a>
+          </div>
+
+          <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul className="nav navbar-nav">
+              <li><a href="/">Redux</a></li>
+            </ul>
+
+          </div>
+        </div>
+      </nav>
+    );
+  }
+
+  _onChange() {
+    this.state = {loggedIn: AuthStore.isLoggedIn()};
+    $(location).attr('href', "/");
+  }
+};
+
+
+NavBar.propTypes = {
+  loggedIn: PropTypes.bool.isRequired
+};
--- web/static/js/rest/components/NaviBar.jsx
+++ web/static/js/rest/components/NaviBar.jsx
@@ -18,7 +18,7 @@ export default class NavBar extends Component {
               <span className="icon-bar"></span>
               <span className="icon-bar"></span>
             </button>
-            <a className="navbar-brand" href="#">Brand</a>
+            <a className="navbar-brand" href="#">Redux</a>
           </div>
 
           <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
